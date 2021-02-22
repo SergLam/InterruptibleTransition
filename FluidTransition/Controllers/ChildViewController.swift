a3e108884c0b107485ca4d1200c1e3be7f693d67
SergLam <s.lamcev@newlinetechnologies.net>
2021-02-22 19:43:26 +0200

    first commit

---
+++ FluidTransition/Controllers/ChildViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ChildViewController.swift
+//  FluidTransition
+//
+//  Created by Mikhail Rubanov on 07/07/2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(hue: 0.2, saturation: 0.8, brightness: 0.9, alpha: 1)
+        view.layer.cornerRadius = 24
+        
+        addDismissButton()
+    }
+    
+    private func addDismissButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Dismiss", for: .normal)
+        button.addTarget(self, action: #selector(dismissSelf), for: .touchUpInside)
+        view.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 80),
+            ])
+    }
+    
+    @objc func dismissSelf() {
+        self.dismiss(animated: true)
+    }
+}
